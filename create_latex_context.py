ee896fbdc7782a94b43aaffe1d1bfbf78fa511ca
Obi-Juan-NoSeEnoje17 <juanvcabreroa@gmail.com>
2024-10-04 11:46:23 +0200

    Cosas

---
+++ create_latex_context.py
@@ -0,0 +1,88 @@
+def points_of_day():
+    latex_content = ""
+    with open ("points_of_day.txt", "r") as points_of_day_read:
+        for point_of_day in points_of_day_read:
+            title = point_of_day.strip().split("-")[0].replace("PDD: ", "")
+            desc = point_of_day.strip().split("-")[1]
+            latex_content = latex_content + "        \\subsection{" + title + "}\n            " + desc + "\n"
+    return latex_content
+
+def create_latex_content(type, date):
+    latex_content = fr"""
+    \documentclass{{article}}
+    \usepackage[utf8]{{inputenc}}
+    \usepackage[utf8]{{inputenc}}
+    \usepackage[left=2.5cm, top=3.5cm, right=2.5cm, bottom=3.5cm]{{geometry}}
+    \usepackage[usenames, dvipsnames, svgnames, table, xcdraw]{{xcolor}}
+    \usepackage[spanish,es-tabla]{{babel}}
+    \usepackage{{float}}
+    \usepackage{{amsmath, amsthm, amssymb, tabu}}
+    \usepackage[breaklinks=true]{{hyperref}} % Referencias
+    \usepackage{{longtable}} \usepackage{{multirow}} \usepackage{{multicol}}
+    \usepackage{{graphicx}} \usepackage{{fancyhdr}}
+    \usepackage{{makeidx}} \usepackage{{enumitem}}
+    \usepackage{{varwidth}} \usepackage{{tasks}}
+    \usepackage{{amsfonts}} \usepackage{{ragged2e}}
+    \usepackage{{amssymb}} % Símbolos en notación matemáticos con el \mathbb
+    \usepackage{{wrapfig}} % Alinear imágenes con respecto al text
+    \usepackage{{caption}} \usepackage{{subcaption}}
+    \usepackage{{upquote}}
+    \usepackage{{color}} \usepackage{{fancyvrb}} 
+    \usepackage{{listings}}
+    \usepackage[numbered,framed]{{matlab-prettifier}}
+    \usepackage{{subfiles}}
+
+    \hypersetup{{colorlinks=true,                     %habilitar colorear enlaces
+    linkcolor=blue,
+    filecolor=magenta,                
+    urlcolor=cyan,
+    }}
+
+    \begin{{document}}
+    \newpage
+
+    \title{{{type}}}
+    \date{{{date}}}
+    \maketitle
+    \begin{{center}}
+        \includegraphics[scale=0.25]{{Logo GUI.jpg}}
+    \end{{center}}
+    \newpage
+    \tableofcontents
+    \newpage
+    \section{{Asistentes}}
+        \subsection{{Lista de asistentes}}
+            \begin{{table}}[h!]
+                \centering
+                \begin{{tabular}}{{c|c}}
+                    Asistentes & Justifican su ausencia \\
+                    & 
+                \end{{tabular}}
+                \label{{tab:asistentes}}
+            \end{{table}}
+    \section{{rden del día}}
+        \subsection{{Aprobación del acta de la sesión anterior}}
+{points_of_day()}
+        
+        
+        
+        
+        
+        
+        
+        \vfill
+    \begin{{center}}Sin más asuntos que tratar queda finalizada la presente Junta \end{{center}}
+    \begin{{center}} Valladolid {date} \end{{center}}
+    \begin{{center}} Constan a continuación la firma del secretario y el presidente con el sello de la asociación\end{{center}}
+
+    \begin{{table}}[htpp]
+    \centering
+    \begin{{tabular}}{{lllll}}
+    El secretario &  & & & El presidente \\
+                &  & & &        
+    \end{{tabular}}
+    \end{{table}}\vspace{{1.5cm}}
+
+    \end{{document}}
+    """
+    return latex_content
---
+++ main.py
@@ -0,0 +1,122 @@
+import telebot
+TOKEN = "7653047311:AAF2We2sxsdvvln-qdg0srosF2JcBO5mS8M" 
+bot = telebot.TeleBot(TOKEN)  
+from roadmap import *
+
+@bot.message_handler(commands=['hola'])
+def say_hello(message):
+    bot.send_message(message.chat.id, "Muy buenas, mi nombre es JuanBot, y he sido creado para generar la plantilla de las juntas y asambleas")
+
+@bot.message_handler(commands=['help'])
+def show_commands(message):
+    command_parts = message.text.split(" ")
+    if len(command_parts) != 1 and len(command_parts) != 2:
+        bot.reply_to(message, "Uso de comando \n/help : muestra ayuda general \n/help comando : muestra ayuda específica")
+    elif len(command_parts) == 1:
+        overall_help = "Comandos existentes : /help /new /add /roadmap /delete \nUtilice /help comando para ver el uso de dicho comando"
+        bot.send_message(message.chat.id, overall_help)
+    elif len(message.text.split(" ")) == 2:
+        commands = {
+            "help" : "/help [comando] \nMuestra ayuda general o de dicho comando \ncomando: comando existente",
+            "new" : "/new \nGenera un nuevo documento para guardar los puntos del día",
+            "add" : "/add punto_del_dia [descripción]\nAñade al documento existente el punto del día con su descripción",
+            "list" : "/list [punto_del_día]\nMuestra todos los puntos del día existentes, sin la descripción, salvo que se indique dicho punto del día",
+            "roadmap" : "/roadmap tipo fecha \nGenera el documento .tex \ntipo: 'Junta' o 'Asamblea' \nfecha: celebración de dicha reunión en formato dd-mm-aaaa",
+            "remove punto_del_día" : "Elimina uno de los puntos del día",
+            "delete" : "Elimina manualmente el archivo de puntos del día. Si se ejecuta /new se reemplaza por otro"
+        }
+        bot.send_message(message.chat.id, commands.get(message.text.split(" ")[1], "Comando desconocido"))
+
+@bot.message_handler(commands=['new'])
+def new_roadmap(message):
+    command_parts = message.text.split(" ")
+    if len(command_parts) != 1:
+        bot.reply_to(message,"Uso de comando: \n/new")
+    else:
+        try:
+            with open("points_of_day.txt", "w") as points_of_day_write:
+                points_of_day_write.write("")
+        except Exception:
+            bot.reply_to(message, "Error al crear archivo de puntos a hablar")
+        bot.send_message(message.chat.id, "Archivo creado")   
+
+
+@bot.message_handler(commands=['add'])
+def add_point_of_day(message):
+    command_parts = message.text.split(" ")
+    if len(command_parts) < 2:
+        bot.reply_to(message,"Uso de comando: \n/add punto_del_dia [descripción]")
+    else:
+        new_point_of_day = command_parts[1]
+        try: 
+            with open('points_of_day.txt', 'r') as points_of_day_read:
+                for point_of_day in points_of_day_read:
+                    if (point_of_day.strip() == "PDD: " + new_point_of_day):
+                        bot.reply_to(message, "Punto del día añadido anteriormente")
+                        return
+        except IOError:
+            bot.reply_to(message, "No existe ningún archivo de puntos del día, puede crearlo con /new")
+
+        with open('points_of_day.txt', 'a') as points_of_day_write:
+            points_of_day_write.write("PDD: " + new_point_of_day + "-")
+            for desc in command_parts[2:]:
+                points_of_day_write.write(desc + " ")
+            points_of_day_write.write("\n")
+        bot.send_message(message.chat.id,  new_point_of_day + " añadido a los puntos del día")
+
+@bot.message_handler(commands=['list'])
+def list_points_of_day(message):
+    command_parts = message.text.split(" ")
+    if len(command_parts) != 1 and len(command_parts) != 2:
+        bot.reply_to(message, "Uso de comando: \n/list [punto_del_día]")
+    elif len(command_parts) == 1:
+        try: 
+            list_points_of_day = "Puntos del diá:\n"
+            with open('points_of_day.txt', 'r') as points_of_day_read:
+                for point_of_day in points_of_day_read:
+                    list_points_of_day = list_points_of_day + point_of_day.split("-")[0].replace("PDD: " , "") + "\n"
+            bot.send_message(message.chat.id, list_points_of_day)
+        except IOError:
+            bot.reply_to(message, "No existe ningún archivo de puntos del día, puede crearlo con /new")
+    else:
+        try:
+            command = command_parts[1] 
+            list_points_of_day = "Descripción de\n"
+            with open('points_of_day.txt', 'r') as points_of_day_read:
+                for point_of_day in points_of_day_read:
+                    if point_of_day.split("-")[0] == "PDD: " + command:
+                        bot.send_message(message.chat.id, point_of_day.split("-")[0] + ": " + point_of_day.split("-")[1])
+        except IOError:
+            bot.reply_to(message, "No existe ningún archivo de puntos del día, puede crearlo con /new")
+        
+    
+
+@bot.message_handler(commands=['remove'])
+def remove_point_of_day(message):
+    command_parts = message.text.split(" ")
+    if len(command_parts) != 2:
+        bot.reply_to(message, "Uso de comando: \n/remove punto_del_día")
+    else:
+        point_of_day_to_remove = command_parts[1]
+        try: 
+            with open('points_of_day.txt', 'r') as points_of_day_read:
+                for point_of_day in points_of_day_read:
+                    if (point_of_day.split("-")[0] == "PDD: " + point_of_day_to_remove):
+                        position = point_of_day
+                        actual_points_of_day = points_of_day_read.readlines()
+                        with open('points_of_day.txt' , 'w') as points_of_day_write:
+                            for point_of_day_eliminate, point_of_day in enumerate(actual_points_of_day):
+                                if point_of_day_eliminate != position:
+                                    points_of_day_write.write(point_of_day)
+        except IOError:
+            bot.reply_to(message, "No existe ningún archivo de puntos del día") 
+
+@bot.message_handler(commands=['delete'])
+def delete(message):
+    bot.reply_to(message.text, "Comando no implementado, en proceso...")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
