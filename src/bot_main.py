256c4505da94228fb48464c31587e639f48ab1cc
Obi-Juan-NoSeEnoje17 <juanvcabreroa@gmail.com>
2024-11-19 11:48:04 +0100

    Added whitelist checks

--- src/bot_main.py
+++ src/bot_main.py
@@ -27,57 +27,75 @@ token =  os.getenv('TOKEN')
 
 bot = telebot.TeleBot(token)
 
+@bot.message_handler(func=lambda message: True)
+def check_junta(message : telebot.Message) -> None:
+    with open("whitelist.txt", 'r') as file_junta:
+        return message.from_user.username in file_junta.readlines()
+
 @bot.message_handler(commands=['hi'])
 def say_hello(message):
-    bot_manager.say_hello(bot, message)
+    if check_junta(message):
+        bot_manager.say_hello(bot, message)
 
 @bot.message_handler(commands=['help'])
 def show_commands(message):
-    bot_manager.show_commands(bot, message)
+    if check_junta(message):
+        bot_manager.show_commands(bot, message)
     
 @bot.message_handler(commands=['incoming'])
 def print_incoming(message):
-    bot_manager.print_incoming(bot, message)
+    if check_junta(message):
+        bot_manager.print_incoming(bot, message)
 
 @bot.message_handler(commands=['changeLogs'])
 def print_change_logs(message):
-    bot_manager.print_changeLogs(bot, message)
+    if check_junta(message):
+        bot_manager.print_changeLogs(bot, message)
 
 @bot.message_handler(commands=['gj'])
 def great_work(message):
-    bot_manager.great_work(bot, message)
+    if check_junta(message):
+        bot_manager.great_work(bot, message)
 
 @bot.message_handler(commands=['aj'])
 def awfull_work(message):
-    bot_manager.awfull_work(bot, message)
+    if check_junta(message):
+        bot_manager.awfull_work(bot, message)
 
 @bot.message_handler(commands=['new'])
 def new_roadmap(message):
-    points_manager.new_roadmap(bot, message)
+    if check_junta(message):
+        points_manager.new_roadmap(bot, message)
 
 @bot.message_handler(commands=['add'])
 def add_point_of_day(message):
-    points_manager.add_point_of_day(bot, message)
+    if check_junta(message):
+        points_manager.add_point_of_day(bot, message)
 
 @bot.message_handler(commands=['list'])
 def list_points_of_day(message):
-    points_manager.list_points_of_day(bot, message)
+    if check_junta(message):
+        points_manager.list_points_of_day(bot, message)
 
 @bot.message_handler(commands=['change'])
 def change_order(message):
-    points_manager.change_order(bot, message)
+    if check_junta(message):
+        points_manager.change_order(bot, message)
 
 @bot.message_handler(commands=['edit'])
 def edit_point_of_day(message):
-    points_manager.edit_point_of_day(bot, message)
+    if check_junta(message):
+        points_manager.edit_point_of_day(bot, message)
 
 @bot.message_handler(commands=['remove'])
 def remove_point_of_day(message):
-    points_manager.remove_point_of_day(bot, message)
+    if check_junta(message):
+        points_manager.remove_point_of_day(bot, message)
     
 @bot.message_handler(commands=['roadmap'])
 def create_roadmap(message):
-    points_manager.create_roadmap(bot, message)
+    if check_junta(message):
+        points_manager.create_roadmap(bot, message)
     
 def run_bot():
     try:
